0b143d3850c838310f466f8370d86ed02dd87268
Kane Zheng <kanez@buildingiq.com>
2020-01-24 13:12:50 +1100

    test

--- src/main/java/com/kanezheng/app/jobmanagement/service/schedule/ScheduleServiceImpl.java
+++ src/main/java/com/kanezheng/app/jobmanagement/service/schedule/ScheduleServiceImpl.java
@@ -42,7 +42,7 @@ public class ScheduleServiceImpl implements ScheduleService {
 	}
 
 	@Override
-	public void deleteSchedule(Long scheduleId) throws Exception {
+	public void deleteSchedule(Long scheduleId) {
 		scheduleRepository.deleteById(scheduleId);
 	}
 
@@ -55,7 +55,7 @@ public class ScheduleServiceImpl implements ScheduleService {
 	}*/
 
 	@Override
-	public void deleteScheduleByWidgetId(String widgetId) throws Exception{
+	public void deleteScheduleByWidgetId(String widgetId){
 		scheduleRepository.deleteByWidgetId(widgetId);
 	}
 }
