3ec621ffcfa4e362b079dd686955e126cf1a5f8e
Kane Zheng <kanez@buildingiq.com>
2020-02-03 10:50:08 +1100

    delete extra files

--- src/main/java/com/kanezheng/app/jobmanagement/config/scheduler/SchedulerConfig.java
+++ src/main/java/com/kanezheng/app/jobmanagement/config/scheduler/SchedulerConfig.java
@@ -1,25 +1,11 @@
 package com.kanezheng.app.jobmanagement.config.scheduler;
 
-import java.io.IOException;
-import java.util.Properties;
-
-import com.kanezheng.app.jobmanagement.AutowiringSpringBeanJobFactory;
-import com.kanezheng.app.jobmanagement.jobs.SimpleJob;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
-import org.quartz.spi.JobFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 
 
 @Configuration
